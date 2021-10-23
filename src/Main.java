a540348dcdcce172c78e46fbaa546d4853d88b33
Romulo <romulojusto@gmail.com>
2021-10-23 12:58:40 -0400

    added anagrams

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,57 @@
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.util.Arrays;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
-
     public static void main(String[] args) {
-        int month=1;
-        int day=15;
-        int year=2015;
+Scanner scan = new Scanner(System.in);
+        System.out.println("Provide a String");
+        String word = scan.nextLine();
+        System.out.println("provide a sub part size");
+        int size = scan.nextInt();
+        scan.close();
+        getSmallestAndLargest(word, size);
 
-        LocalDate time = LocalDate.of(year, month, day);
 
-        String DayValue = String.valueOf(time.getDayOfWeek());
-
-        System.out.println(DayValue);
 
     }
 
+    public static void getSmallestAndLargest(String s, int k){
+        String smallest;
+        String largest;
+        String temp;
+
+
+        ArrayList<String> collection = new ArrayList<>();
+
+        for(int i = 0; i < s.length() - 3;i++){
+            temp = s.substring(i,i+k);
+            collection.add(temp);
+        }
+
+        for (String item: collection
+             ) {
+            System.out.println(item.toString());
+        }
+
+        for(int j = 0; j < collection.size() - 1; j++){
+            if(collection.get(j).compareTo(collection.get(j + 1))> 0){
+                swap(collection.get(j), collection.get(j + 1));
+            }
+        }
+
+        smallest = collection.get(0);
+        largest = collection.get(collection.size()-1);
+
+        System.out.println("The Smallest char: " + smallest);
+        System.out.println("The Largest char: " + largest);
+    }
+
+    public static void swap(String a, String b){
+        String temp = a;
+        a = b;
+        b = temp;
+    }
 }
