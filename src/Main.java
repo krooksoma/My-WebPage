c4f676a2d8de854322f021db8810aaa52a112501
Romulo <romulojusto@gmail.com>
2021-10-27 09:04:21 -0400

    Working with 2d Arrays-Hourglass sum

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Locale;
@@ -5,13 +9,20 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        String myString = " This is my very long string";
-        String[] newArray = myString.split(" ");
+    public static void main(String[] args) throws IOException {
 
-        for(String a: newArray){
-            System.out.println(a);
-        }
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        BigInteger a = new BigInteger(reader.readLine());
+        BigInteger b = new BigInteger(reader.readLine());
+
+        BigInteger c = a.add(b);
+        BigInteger d = a.multiply(b);
+
+        reader.close();
+
+        System.out.println(c);
+        System.out.println(d);
+
     }
-
 }
