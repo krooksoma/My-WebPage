5c5f1fbc0baa684de01fbf56801715c216d7f4a6
Romulo <romulojusto@gmail.com>
2021-11-02 18:20:38 -0400

    Working with 2D arrays and HashMaps

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,60 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
+import java.rmi.NotBoundException;
 import java.text.NumberFormat;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException{
+        public static void main(String[] args) throws IOException{
+//            get user input
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("provide string to check for palindrome");
+//            reads value from input and assign it to a variable
+            String stringOne = reader.readLine();
+            String stringTwo = reader.readLine();
+            reader.close();
 
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//            anagram
 
-        int  n = Integer.parseInt(bufferedReader.readLine().trim());
 
-        String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
 
-        List<Integer> arr = new ArrayList<>();
-
-        for (int i = 0; i < n; i++){
-            int arrItem = Integer.parseInt(arrTemp[i]);
-            arr.add(arrItem);
         }
 
-        bufferedReader.close();
+        public static boolean checkAnagram(String one, String two){
+            int sum = 0;
 
-        for(int i = 0; i < n; i ++){
-            System.out.print(arr.get(i) + " ");
+//            check strings length first(1st constraint)
+            if(one.length() != two.length()){
+                return false;
+            }
+//            convert both to lowercase(constraint 2)
+            two = two.toLowerCase();
+            one = one.toLowerCase();
+
+//            loop through each character on the alphabet
+            for(int c = 'a'; c <='z'; c++){
+//             loop through each string
+                for(int i = 0; i < one.length(); i++){
+//                    string one
+                    if(one.charAt(i) == c){
+                        sum ++;
+                    }
+//                    string two
+                    if(two.charAt(i) == c){
+                        sum--;
+                    }
+                }
+//                if the sum does not end up as zero it means the string are not anagrams
+                if(sum != 0){
+                    return false;
+                }
+            }
+            return true;
         }
-    }
-
-
 }
+
