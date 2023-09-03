240a4c86e5aedcc05022b65faca1765622237cb0
Romulo <romulojusto@gmail.com>
2023-09-03 06:57:53 -0400

    added new functionality to webpage

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,17 +1,19 @@
 
 function Pizza({ pizzaObj }) {
 
-  if(pizzaObj.soldOut){
-    return null;
-  }
+  //conditional rendering of the element
+  // if(pizzaObj.soldOut){
+  //   return null;
+  // }
 
   return (
-    <li className="pizza">
+    // conditionally setting css property
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img alt={pizzaObj.name} src={pizzaObj.photoName} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.price}</span>
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
       </div>
     </li>
   );
